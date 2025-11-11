76265d4cb8562d8cb0d1fe56d951c85fce1b0860
Andra Petcu <andra-maria.petcu@endava.com>
2025-11-11 16:11:37 +0200

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+#A view is a place where we put the "logic" of our application.
+# 1.Receive the request information as well as parameters parsed from the url.
+# 2.Fetch the information from the model, probably adding some logic.
+# 3.Create a response by filling a template with the fetched info.
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
